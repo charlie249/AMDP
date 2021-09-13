89383238c849e2c95a941e953e0c30071e8bd17a
Charlie <charlie@deepbluec.com>
2021-09-13 09:23:09 +0100

    Making the form

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 async function getUserData() {
 
-    var issueNumb = prompt("Please enter the number of the issue you would like to view information about: ");
+    //var issueNumb = prompt("Please enter the number of the issue you would like to view information about: ");
+    //document.getElementById('issue_numb').innerHTML;
+    //console.log(issueNumb); 
+
+    var issueNumb = document.getElementById('issue_numb').value;
+    var repoName = document.getElementById('repo_name').value; 
+    console.log(issueNumb);
 
     const issue = await fetch('https://api.github.com/repos/serge-web/serge/issues/' + issueNumb);
     const issueData = await issue.json();
@@ -10,7 +16,6 @@ async function getUserData() {
     document.querySelector('#state').innerHTML = `<strong> State: </strong> ${issueData.state}`;   
     document.querySelector('#created').innerHTML = `<strong> Created at: </strong> ${issueData.created_at}`;
     document.querySelector('#closed').innerHTML = `<strong> Closed at: </strong> ${issueData.closed_at}`;
-
 
     const date1 = new Date(issueData.created_at);
     const date2 = new Date(issueData.closed_at);
@@ -37,6 +42,28 @@ async function myFunction(){
 
 }
 
+function main() {
+    // Adds event listenener to form with id of repo_info
+    var myForm = document.getElementById("repo_info");
+    // A function called validateForm will be called when the form submits
+    myForm.addEventListener("submit", validateForm);
+}
+
+function validateForm(event){
+    var formValid = true;
+    event.preventDefault();
+    var myForm = document.getElementById("repo_info");
+
+    if(myForm.question.value == ""){
+        formValid = False;
+        //Show Error message
+        document.getElementById("repo_nameRequiredError").style.display = "block";
+        //Stop form submitting
+        event.preventDefault();
+    } else {
+        document.getElementById("repo_nameRequiredError").style.display = "none";
+    }
+}
 
 window.addEventListener('DOMContentLoaded', () => {
     getUserData();
