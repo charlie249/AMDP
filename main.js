4c265fc9750a85c4e7d4136b35244221ff5c48f3
Charlie <charlie@deepbluec.com>
2021-09-14 14:41:44 +0100

    Corrected code

--- main.js
+++ main.js
@@ -30,48 +30,6 @@ async function getUserData() {
 
 }
 
-async function repoName(){
-
-    var userInput = prompt("Please enter the name of the repository you would like to view information about: ");
-    const repoName = await fetch('https://api.github.com/repos/serge-web/' + userInput);
-    const repoNameData = await repoName.json();
-    console.log(repoNameData);
-
-    document.querySelector('#trial').innerHTML = `<strong> Issues: </strong> ${repoNameData.issues_urls}`;
-
-
-    /* const date1 = new Date(repoNameData.created_at);
-    const date2 = new Date(issueData.closed_at);
-
-    if (issueData.closed_at != null){
-        const diffTime = Math.abs(date2 - date1);
-        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 *24));
-        document.querySelector('#days').innerHTML = `<strong> Length of time the issue was open: </strong> ${diffDays} days`; 
-    }
-    else {
-        document.querySelector('#days').innerHTML = `<strong> This issue has not been closed yet </strong>`;
-     } */
-}
-
-function parseURLParams(url) {
-    var queryStart = url.indexOf("?") + 1,
-        queryEnd   = url.indexOf("#") + 1 || url.length + 1,
-        query = url.slice(queryStart, queryEnd - 1),
-        pairs = query.replace(/\+/g, " ").split("&"),
-        parms = {}, i, n, v, nv;
-
-    if (query === url || query === "") return;
-
-    for (i = 0; i < pairs.length; i++) {
-        nv = pairs[i].split("=", 2);
-        n = decodeURIComponent(nv[0]);
-        v = decodeURIComponent(nv[1]);
-
-        if (!parms.hasOwnProperty(n)) parms[n] = [];
-        parms[n].push(nv.length === 2 ? v : null);
-    }
-    return parms;
-}
 
 async function repoName(){
 
