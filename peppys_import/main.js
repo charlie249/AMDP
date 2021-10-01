f078f512dd5c6e147da46b9a12ec5f3626752801
Charlie <charlie@deepbluec.com>
2021-10-01 09:25:08 +0100

    Fixed the "merged_at" segment

--- peppys_import/main.js
+++ peppys_import/main.js
@@ -45,55 +45,97 @@ async function getPullRequest() {
 
     for(i = 0; i < pulls.length; i++)
     {
-        document.querySelector('#userLogin').innerHTML = `<strong> User Login: </strong> ${pulls[i].user.login}`;
-
-<<<<<<< HEAD
         if(pulls[i].merged_at == null )
-=======
-        if(pulls[i].merged_at = 'null' )
->>>>>>> master
         { 
             var ol = document.getElementById("pull");
             var num = document.createElement("li");
             var title = document.createElement("li");
             var userLogin = document.createElement("li");
-            var createdAt = document.createElement("li");
+            var closedAt = document.createElement("li");
             var files = document.createElement("li");
             var merged = document.createElement("li");
             var space = document.createElement("p");
             num.appendChild(document.createTextNode(pulls[i].number));
             title.appendChild(document.createTextNode(pulls[i].title));
             userLogin.appendChild(document.createTextNode(pulls[i].user.login));
-            createdAt.appendChild(document.createTextNode(pulls[i].created_at));
+            closedAt.appendChild(document.createTextNode(pulls[i].closed_at));
             files.appendChild(document.createTextNode(pulls[i].files));
             merged.appendChild(document.createTextNode("This pull request has not yet been merged!"));
             ol.appendChild(num);
             ol.appendChild(title);
             ol.appendChild(userLogin);
-            ol.appendChild(createdAt);
+            ol.appendChild(closedAt);
             ol.appendChild(files);
             ol.appendChild(merged);
             ol.appendChild(space);
 
-        }else {
+        } else{
             var ol = document.getElementById("pull");
             var num = document.createElement("li");
             var title = document.createElement("li");
             var userLogin = document.createElement("li");
-            var createdAt = document.createElement("li");
+            var closedAt = document.createElement("li");
             var files = document.createElement("li");
             var merged = document.createElement("li");
             var space = document.createElement("p");
             num.appendChild(document.createTextNode(pulls[i].number));
             title.appendChild(document.createTextNode(pulls[i].title));
             userLogin.appendChild(document.createTextNode(pulls[i].user.login));
-            createdAt.appendChild(document.createTextNode(pulls[i].created_at));
+            closedAt.appendChild(document.createTextNode(pulls[i].closed_at));
             files.appendChild(document.createTextNode(pulls[i].files));
             merged.appendChild(document.createTextNode(pulls[i].merged_at));
             ol.appendChild(num);
             ol.appendChild(title);
             ol.appendChild(userLogin);
-            ol.appendChild(createdAt);
+            ol.appendChild(closedAt);
+            ol.appendChild(files);
+            ol.appendChild(merged);
+            ol.appendChild(space);
+        }
+
+        if(pulls[i].closed_at == null )
+        { 
+            var ol = document.getElementById("pull");
+            var num = document.createElement("li");
+            var title = document.createElement("li");
+            var userLogin = document.createElement("li");
+            var closedAt = document.createElement("li");
+            var files = document.createElement("li");
+            var merged = document.createElement("li");
+            var space = document.createElement("p");
+            num.appendChild(document.createTextNode(pulls[i].number));
+            title.appendChild(document.createTextNode(pulls[i].title));
+            userLogin.appendChild(document.createTextNode(pulls[i].user.login));
+            closedAt.appendChild(document.createTextNode("This pull request has not yet been merged!"));
+            files.appendChild(document.createTextNode(pulls[i].files));
+            merged.appendChild(document.createTextNode(pulls[i].merged_at));
+            ol.appendChild(num);
+            ol.appendChild(title);
+            ol.appendChild(userLogin);
+            ol.appendChild(closedAt);
+            ol.appendChild(files);
+            ol.appendChild(merged);
+            ol.appendChild(space);
+
+        } else{
+            var ol = document.getElementById("pull");
+            var num = document.createElement("li");
+            var title = document.createElement("li");
+            var userLogin = document.createElement("li");
+            var closedAt = document.createElement("li");
+            var files = document.createElement("li");
+            var merged = document.createElement("li");
+            var space = document.createElement("p");
+            num.appendChild(document.createTextNode(pulls[i].number));
+            title.appendChild(document.createTextNode(pulls[i].title));
+            userLogin.appendChild(document.createTextNode(pulls[i].user.login));
+            closedAt.appendChild(document.createTextNode(pulls[i].closed_at));
+            files.appendChild(document.createTextNode(pulls[i].files));
+            merged.appendChild(document.createTextNode(pulls[i].merged_at));
+            ol.appendChild(num);
+            ol.appendChild(title);
+            ol.appendChild(userLogin);
+            ol.appendChild(closedAt);
             ol.appendChild(files);
             ol.appendChild(merged);
             ol.appendChild(space);
@@ -101,21 +143,7 @@ async function getPullRequest() {
     }
 
 }
-
 /*
-    console.log('in my')
-    const repo = document.querySelector('#repo').value
-    const issue = document.querySelector('#issue').value
-
-    const date1 = new Date(issueData.created_at);
-    const date2 = new Date(issueData.closed_at);
-
-    if (issueData.closed_at != null){
-        const diffTime = Math.abs(date2 - date1);
-        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 *24));
-        document.querySelector('#days').innerHTML = `<strong> This issue was open for: </strong> ${diffDays} days`; 
-    }
-
 window.addEventListener('DOMContentLoaded', () => {
     getUserData();
 }); */
