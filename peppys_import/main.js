047994b94c0d4cf6247606d5caeb962841266876
Charlie <charlie@deepbluec.com>
2021-10-04 13:21:43 +0100

    Fixed merge

--- peppys_import/main.js
+++ peppys_import/main.js
@@ -5,7 +5,7 @@ async function getUserData() {
     const repoName = await fetch('https://api.github.com/repos/' + repo);
     const repoNameData = await repoName.json();
 
-    const pullsData = await fetch('https://api.github.com/repos/' + repo + '/pulls');
+    const pullsData = await fetch('https://api.github.com/repos/' + repo + '/pulls?state=all');
     const pulls = await pullsData.json();
 
     for(i = 0; i < pulls.length; i++)
@@ -18,19 +18,11 @@ async function getUserData() {
             numb = pulls.length;
     }
 
-   // const mergesData = await fetch('https://api.github.com/repos/' + repo + '/merges');
-   // const merges = await mergesData.json();
-
-
-    //var issuesJSON = JSON.parse(issues);
-    //console.log(issuesJSON);
-
     document.querySelector('#name').innerHTML = `<strong> Name of repository: </strong> ${repoNameData.name}`;
     document.querySelector('#description').innerHTML = `<strong> Description: </strong> ${repoNameData.description}`;
     document.querySelector('#updated').innerHTML = `<strong> Last Updated At: </strong> ${repoNameData.updated_at}`;
     document.querySelector('#subscribers').innerHTML = `<strong> Number of Subscribers: </strong> ${repoNameData.subscribers_count}`;
-    document.querySelector('#pulls').innerHTML = `<strong> Pulls: </strong>`;
-    document.querySelector('#merges').innerHTML = `<strong> Merges: </strong>`;
+    document.querySelector('#pulls').innerHTML = `<strong> Pulls: </strong> ${pulls[i]}`;
     document.querySelector('#totalIssues').innerHTML = `<strong> Number of issues: </strong> ${numb}`;
     }
 
@@ -38,18 +30,11 @@ async function getPullRequest() {
 
     const repo = document.querySelector('#repo').value;
 
-    const pull= document.querySelector('#pulls').value
-
     const pullsData = await fetch('https://api.github.com/repos/' + repo + '/pulls?state=all');
     const pulls = await pullsData.json();
 
     for(i = 0; i < pulls.length; i++)
     {
-<<<<<<< HEAD
-=======
-        document.querySelector('#userLogin').innerHTML = `<strong> User Login: </strong> ${pulls[i].user.login}`;
-
->>>>>>> master
         if(pulls[i].merged_at == null )
         { 
             var ol = document.getElementById("pull");
